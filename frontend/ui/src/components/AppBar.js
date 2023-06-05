817efc0e589de5fb7662c7537c7a71aa112d135d
Lander Van den Bulcke <landervdb@inuits.eu>
2023-06-05 16:28:29 +0200

    fix: fix frontend errors
    Signed-off-by: Lander Van den Bulcke <landervdb@inuits.eu>

--- frontend/ui/src/components/AppBar.js
+++ frontend/ui/src/components/AppBar.js
@@ -1,6 +1,5 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { AppBar, Box, Toolbar, Typography, Button, Link } from "@mui/material";
-import { Menu as MenuIcon } from "@mui/icons-material";
 import { AuthContext } from "../contexts/auth-context";
 
 function CustomAppBar() {
@@ -12,8 +11,8 @@ function CustomAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: 900, fontFamily: 'Montserrat' }}>
             O11y
           </Typography>
-      {!authState.token &&
-          <Button color="inherit" component={Link} to="/login">Login</Button>
+      {(!authState.token &&
+          <Button color="inherit" component={Link} to="/login">Login</Button>)
           ||
           <Button color="inherit" onClick={logout}>Logout</Button>
       }
